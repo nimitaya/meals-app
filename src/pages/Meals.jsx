b555c7ad141269c5e6e2ff826d9ae1fa7b123656
nimitaya <sonja.schweren@web.de>
2025-02-25 16:19:53 +0100

    add fetch API

--- src/pages/Meals.jsx
+++ src/pages/Meals.jsx
@@ -1,11 +1,34 @@
+import { useEffect, useState } from "react";
 import CardItem from "../components/CardItem";
+import { fetchData } from "../utilities/apiFetch";
 
 const Meals = () => {
-  const mealsUrl = "https://www.themealdb.com/api/json/v1/1/search.php?f=p"
+  const [meals, setMeals] = useState([]);
+
+  const mealsUrl = "https://www.themealdb.com/api/json/v1/1/search.php?f=p";
+
+  useEffect(()=>{
+    async function fetchMeals(){
+      try {
+        const data = await fetchData(mealsUrl)
+        console.log(data.meals);
+        setMeals(data.meals)
+      } catch (error) {
+        console.error(error.message)
+      }
+    }
+    fetchMeals()
+  }, [])
+
 
   return (
     <main data-theme="night" className="container mx-auto p-4">
-      <CardItem />
+      {
+        meals.length > 0 ? (
+          meals.map((food)=> <p key={food.idMeal}>{food.strMeal}</p>)
+        ) : <p>No meals available right now.</p>
+      }
+      {/* <CardItem name={food.strMeal} pic={food.strMealThumb} /> */}
     </main>
   );
 };
